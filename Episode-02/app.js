8f0fd1b0c1545524d875b1fa8a60708edef66544
kishore <kishore.krrish13@gmail.com>
2025-01-07 13:54:53 +0530

    episode-02

---
+++ Episode-02/app.js
@@ -0,0 +1,4 @@
+const heading = document.createElement("h1")
+heading.innerHTML = "hello javascript"
+const divElement = document.getElementById("root")
+divElement.appendChild(heading);
---
+++ Episode-02/app1.js
@@ -0,0 +1,3 @@
+const abc  = React.createElement("h1",{},"Hello react")
+const root = RootDoM.createRoot(document.getElementById("root"))
+root.render(abc)
